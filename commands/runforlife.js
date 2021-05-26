e832df1bbb17b90955e5296edb51925dd3037447
Bat-Husky <79540314+Bat-Husky@users.noreply.github.com>
2021-05-26 19:20:56 +0200

    Patch

--- commands/runforlife.js
+++ commands/runforlife.js
@@ -4,10 +4,10 @@ const { Client, MessageEmbed } = require('discord.js');
 const run = new Map();
 const list = new Set();
 
-module.exports = class RunForLife extends commands {
+module.exports = class Run extends commands {
 
     static match (message, prefix) {
-        return message.content.toString().toLowerCase().startsWith(`${prefix}run`)
+        return message.content.toString().toLowerCase().startsWith(`${prefix}run`);
     }
 
     static action (message) {
@@ -15,11 +15,10 @@ module.exports = class RunForLife extends commands {
         if (message.content.toString().toLowerCase().split(' ')[1] == "info") return this.info(message);
         if (message.content.toString().toLowerCase().split(' ')[1] == "list") return this.list(message);
 
-        let runName = message.content.toString().split(' ')
-        runName.shift()
-        runName.shift()
-        runName = runName.join(' ')
-        if (!runName) return message.reply("Il n'y a pas de nom de run.")
+        let runName = message.content.toString().split(' ');
+        runName.splice(0, 2);
+        runName = runName.join(' ');
+        if (!runName) return message.reply("Il n'y a pas de nom de run.");
 
         if (message.content.toString().toLowerCase().split(' ')[1] == "create") return this.create_run(message, runName);
         if (message.content.toString().toLowerCase().split(' ')[1] == "join") return this.join_run(message, runName);
@@ -32,7 +31,7 @@ module.exports = class RunForLife extends commands {
               .setColor("#4CD477")
               .setTitle("Run commands")
               .setDescription("Cette commande permet de créer des courses pour vous départager.")
-              .addField("Utilisation :", "`$run info` : cette commande. \n `$run create` : $run create <name> \n `$run start` : $run start <name> ; creator only \n `$run delete` : $run delete <name> ; creator only \n `$run join` : $run join <name>")
+              .addField("Utilisation :", "`$run info` : cette commande. \n`$run list` : Donne la liste des runs ouvertes. \n `$run create` : $run create <name> \n `$run start` : $run start <name> ; creator only \n `$run delete` : $run delete <name> ; creator only \n `$run join` : $run join <name>")
         message.channel.send(embed)
     }
 
@@ -49,13 +48,13 @@ module.exports = class RunForLife extends commands {
         const runInfo = {
             run_name: runName,
             participant: [],
-            creator: message.author
+            creator: message.author,
+            isStarted: false
         }
 
-        runInfo.participant.push(message.author);
+        // runInfo.participant.push(message.author);
 
         run.set(runName, runInfo);
-
         list.add(runName);
 
         message.channel.send(`The run **${runName}** has been created`);
@@ -70,7 +69,6 @@ module.exports = class RunForLife extends commands {
         }
 
         runInfo.participant.push(message.author);
-        
         message.reply(`You joined the run **${runName}**`);
     }
 
@@ -78,15 +76,20 @@ module.exports = class RunForLife extends commands {
         if (!run.get(runName)) return message.reply("This run doesn't exist.");
         const runInfo = run.get(runName);
         if (message.author != runInfo.creator) return message.reply("You are not the creator of this run!");
+        if (runInfo.participant.length == 0 || runInfo.participant.length == 1) return message.reply("There are not enough people!")
+        if (runInfo.isStarted == true) return message.reply("This run is already started.");
+        runInfo.isStarted = true;
         
         message.channel.send("The run start!");
 
-        let nbWinner = Math.floor(Math.random() * runInfo.participant.length);
+        let random = Math.random()
+        let nbWinner = Math.floor(random * runInfo.participant.length);
         let Winner = runInfo.participant[nbWinner];
 
         setTimeout(() => {
-            message.channel.send(`The winner is **${Winner}** !`)
-            this.delete_run(message, runName, true)
+            runInfo.isStarted = false;
+            message.channel.send(`The winner is **${Winner}** !`);
+            this.delete_run(message, runName, true);
         }, 6000);
     }
 
@@ -94,9 +97,10 @@ module.exports = class RunForLife extends commands {
         if (!run.get(runName)) return message.reply("This run doesn't exist.");
         const runInfo = run.get(runName);
         if (message.author != runInfo.creator) return message.reply("You are not the creator of this run!");
+        if (runInfo.isStarted == true) return message.reply("This run is started.");
 
         run.delete(runName);
-        list.delete(runName)
+        list.delete(runName);
 
         if (isFinished == false) return message.reply("The run has been deleted !");
     }
