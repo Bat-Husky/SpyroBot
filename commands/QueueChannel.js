95f9957212a06ae186928855c083986c949ca256
Bat-Husky <79540314+Bat-Husky@users.noreply.github.com>
2021-05-29 19:26:59 +0200

    Update QueueChannel.js

--- commands/QueueChannel.js
+++ commands/QueueChannel.js
@@ -13,7 +13,7 @@ module.exports = class QueueChannel extends commands {
     static action (message) {
         if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You can't use that command!");
 
-        let QueueChannel = JSON.parse(fs.readFileSync("../ReBot_test/JSON/QueueChannel.json", "utf8"));
+        let QueueChannel = JSON.parse(fs.readFileSync("./JSON/QueueChannel.json", "utf8"));
 
         
         if (!message.content.toString().split(' ')[1]) return message.reply("Définnissez le channel de la Queue comme ceci : \n`SetQueueChannel <id or name>`")
@@ -23,7 +23,7 @@ module.exports = class QueueChannel extends commands {
 
         if (!message.guild.channels.cache.find(ch => ch.name == QueueChannel[message.guild.id]) && !message.guild.channels.cache.find(ch => ch.id == QueueChannel[message.guild.id])) return message.reply("Ce channel n'existe pas")
 
-        fs.writeFile("../ReBot_test/JSON/QueueChannel.json", JSON.stringify(QueueChannel), (err) => {
+        fs.writeFile("./JSON/QueueChannel.json", JSON.stringify(QueueChannel), (err) => {
             if (err) {
                 console.log(err);
             }
