022f0a98e3a1af993c9e2f298faeac6a03442acb
Bat-Husky <79540314+Bat-Husky@users.noreply.github.com>
2021-05-31 10:02:11 +0200

    Add files via upload

--- commands/malfoutu.js
+++ commands/malfoutu.js
@@ -12,9 +12,34 @@ module.exports = class MalFoutu extends commands {
 
     static action (message) {
         let status = JSON.parse(fs.readFileSync("./JSON/CommandStatus.json", "utf8"));
+        let malfoutu = JSON.parse(fs.readFileSync("./JSON/malfoutu.json", "utf8"));
 
         if (status["malfoutu"][message.guild.id] == "off") return;
+
+        if (message.content.toString().toLowerCase().split(' ')[1] == "set") return this.set(message, malfoutu);
         
+        if (malfoutu[message.author.id]) return message.channel.send(`${malfoutu[message.author.id]}`, {tts: true})
         message.channel.send('Tu te calme martine ! Tu crois ça facile de coder un bot ?', {tts: true})
+    }
+
+    static set (message, malfoutu) {
+        let coins = JSON.parse(fs.readFileSync("./JSON/coin.json", "utf8"));
+        if (coins[message.author.id].coins < 750) return message.reply(`You don't have enough coins to change your text of \`$tonbotestmalfoutu\`! \n\`${coins[message.author.id].coins}\` < \`750\``)
+
+        if (!message.content.toString().toLowerCase().split(' ')[2]) return message.reply("Vous n'avez pas mis de phrase.")
+
+        let newPhrase = message.content.toString().split(' ')
+        newPhrase.splice(0, 2)
+        newPhrase = newPhrase.join(' ')
+
+        malfoutu[message.author.id] = newPhrase;
+
+        fs.writeFile("./JSON/malfoutu.json", JSON.stringify(malfoutu), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+
+        message.channel.send(`$Tonbotestmalfoutu now set to : \n\`${newPhrase}\``)
     }
 }
