21a1da4a3166a8818befeb1df70c27fa1517b321
Bat-Husky <79540314+Bat-Husky@users.noreply.github.com>
2021-07-15 21:27:23 +0200

    Add files via upload

--- commands/SpyroBot.js
+++ commands/SpyroBot.js
@@ -2,30 +2,63 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const commands = require('./commands');
 const { Client, MessageEmbed } = require('discord.js');
-const djsImg = new Discord.MessageAttachment('../ReBot_test/Img/discord-js.png')
-const ppImg = new Discord.MessageAttachment('../ReBot_test/Img/spyrobot_v1.png')
+const djsImg = new Discord.MessageAttachment('./Img/discord-js.png')
+const ppImg = new Discord.MessageAttachment('./Img/spyrobot_v1.png')
 
-module.exports = class SpyroBot extends commands {
+module.exports = class SpyroBot {
 
-    static match (message, prefix) {
-        return message.content.toString().toLowerCase().startsWith(`${prefix}spyrobot`)
+    static parse (message, prefix, bot) {
+        if (this.match(message, prefix)) {
+            this.action(message, prefix, bot)
+            return true
+        }
+        return false
     }
 
-    static action (message) {
+    static match (message, prefix) {
+        return message.content.toString().toLowerCase().startsWith(`${prefix}spyro`)
+    }
+
+    static action (message, prefix, bot) {
+        if (message.content.toString().toLowerCase().split(' ')[1] == "code") return this.code(message);
+        if (message.content.toString().toLowerCase().split(' ')[1] == "github") return this.GitHub(message);
+        if (message.content.toString().toLowerCase().split(' ')[1] == "history") return this.History(message);
+
+        if (!message.content.toString().toLowerCase().startsWith(`${prefix}spyrobot`)) return;
+
         const embed = new MessageEmbed()
               .setColor("#0042ff")
               .setTitle("SpyroBot")
               .setURL("https://docs.google.com/document/d/1uSBdN_1_jUk0arHGbWB0kMjv6YJGQTgoLKO2QhjjhK8/edit?usp=sharing")
               .setDescription("Le Grand Bot SpyroBot")
-              .setThumbnail('https://media.discordapp.net/attachments/575712614097879050/799944045778436107/spyrobot_v1.png?width=670&height=670')
+              .setThumbnail(bot.user.avatarURL("png"))
               .addField("Créateur :", "[Bat Husky](https://discord.gg/kcb3jke)")
               .addField('\u200b', '\u200b')
               .addFields(
-                { name: 'Langage :', value: 'javascipt', inline: true },
-                { name: 'Librairie :', value: 'discord.js', inline: true }
+                { name: 'Langage :', value: 'javascript', inline: true },
+                { name: 'Librairie :', value: 'discord.js', inline: true },
+                { name: 'Préfix :', value: '`$`', inline: true }
               )
               .attachFiles(djsImg)
               .setImage('attachment://discord-js.png')
         message.channel.send(embed);
+        // message.channel.send({ embeds: [embed] });
+        // TODO : v13
+    }
+
+    static code (message) {
+        const embed = new MessageEmbed()
+            .setTitle("SpyroBot")
+            .setColor("#5465FF")
+            .setDescription("SpyroBot est codé en JavaScript avec la librairie Discord.js, voici un exemple de code pour les intéressés : \n\n```js\nconst commands = require('./commands');\nconst { Client, MessageEmbed } = require('discord.js');\nconst fs = require('fs');\n\n\nmodule.exports = class Infractions extends commands {\n\n    static match (message, prefix) {\n        return message.content.toString().toLowerCase().startsWith(`${prefix}infractions`)\n    }\n\n    static action (message) {\n      let warns = JSON.parse(fs.readFileSync(\"../ReBot_test/JSON/Warning.json\", \"utf8\"));\n\n      if(!message.member.permissions.has(\"MANAGE_MEMBERS\")) return message.reply(\"You can't use that command!\");\n      let wUser = message.guild.member(message.mentions.users.first()) || message.mentions.users.first();\n      if(!wUser) return message.reply(\"Can't find user!\");\n\n      var warned = message.mentions.users.first();\n\n      if(!warns[wUser.id]) return message.reply(\"This user has no warn!\");\n\n      const warnEmbed = new MessageEmbed()\n          .setTitle('Infractions')\n          .setColor(\"#0042ff\")\n          .addField(\"User\", warned)\n          .addField(\"Number of Warnings\", warns[wUser.id].warns)\n      message.channel.send(warnEmbed);\n    }\n}\n```")
+        message.channel.send(embed)
+    }
+
+    static GitHub (message) {
+        message.channel.send("Le GitHub de SpyroBot : \nhttps://github.com/Bat-Husky/SpyroBot")
+    }
+
+    static History (message) {
+        message.channel.send("Voici L'Historique de SpyroBot : \nhttps://docs.google.com/document/d/1VJSgnw-fHwb26KB1TmyZVageVWFeAg_D-pFDm0Z_G0E/edit?usp=sharing")
     }
 }
